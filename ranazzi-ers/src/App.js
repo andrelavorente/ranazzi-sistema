cc9bb16b4bcafdf4f55ff1669bde58a573bd65c7
André Lavorente <lavorente.a@gmail.com>
2023-11-13 23:05:51 -0300

    primeiro commit

--- ranazzi-ers/src/App.js
+++ ranazzi-ers/src/App.js
@@ -1,13 +1,17 @@
-import "./App.css";
-import Login from "./pages/Login/login.js";
-import RoutesApp from "./routes.js";
+import React from "react";
+import GlobalStyle from "./styles/global.js";
+import RoutesApp from "./routes";
+import { AuthProvider } from "./contexts/auth.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <AuthProvider>
+        <RoutesApp />
+        <GlobalStyle />
+      </AuthProvider>
+    </>
   );
-}
+};
 
 export default App;
